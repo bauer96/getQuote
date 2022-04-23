ee5fce598626f74d03ac70fcb20600581e7bb1a8
bauer96 <zocken210@web.de>
2022-04-24 00:35:29 +0200

    createt the complete HTML Structuce, add some Css and the function in JS

---
+++ Quote.js
@@ -0,0 +1,16 @@
+
+const quoteText = document.querySelector(".quote"),
+quoteBtn = document.querySelector("button"),
+authorName = document.querySelector(".name")
+
+function newRandomQuote(){
+   
+    fetch("http://api.quotable.io/random").then(response => response.json()).then(result => {
+        quoteText.innerText = result.content;
+        authorName.innerText = result.author;
+        quoteBtn.classList.remove("loading");
+        quoteBtn.innerText = "getQuote";
+    });
+}
+
+quoteBtn.addEventListener("click", newRandomQuote);
